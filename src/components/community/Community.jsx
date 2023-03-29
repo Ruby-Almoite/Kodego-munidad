ffcca9183a83849c65b2806626c7e96e90db4641
Ruby-Almoite <ruby.almoite22@gmail.com>
2023-03-30 07:25:43 +0800

    For Presentation

---
+++ src/components/community/Community.jsx
@@ -0,0 +1,13 @@
+import Tab from './/tabs/Tab'
+import './community.css'
+
+export default function Community() {
+  return (
+    <div className ="communityPage">
+        <span className ="communityTitleLg"> Our Community </span>
+<Tab/>
+
+      
+    </div>
+  )
+}
